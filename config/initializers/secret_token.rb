7b74b99e02a7c16db62d1b47ca21a26f02e511ad
gold_river <goldriver@goldriver-System-Product-Name.(none)>
2013-03-25 21:04:34 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'bddb4fb5ba6ecede611206ad18ab67b1792267280e85e65421cc28fbb2f6f10ffd5262b3b9058385c74438e9cbd9c658eba7b399cbe928e16d00f72d01988287'
